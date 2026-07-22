3c10e5a8e6dc29ad84d1f302ffca5b0cda154038
Shivam <shivam@example.com>
2026-07-22 22:15:26 +0530

    final

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,149 +1,43 @@
 import React, { useEffect } from 'react';
 import Layout from '../components/Layout';
+import { Link } from 'react-router-dom';
+
 import asset_Bulk_Flex_Brand_white_1024x293_png_1 from '../assets/images/Bulk_Flex_Brand_white-1024x293.png?url';
 import asset_Bulk_Flex_Brand_white_300x86_png_2 from '../assets/images/Bulk_Flex_Brand_white-300x86.png?url';
 import asset_Bulk_Flex_Brand_white_768x220_png_3 from '../assets/images/Bulk_Flex_Brand_white.png?url';
 import asset_simplified_text_1024x146_png_4 from '../assets/images/simplified_text-1024x146.png?url';
-import asset_hooks_min394d_js_5 from '../assets/wp-includes/js/dist/hooks.min394d.js?url';
-import asset_i18n_mineca5_js_6 from '../assets/wp-includes/js/dist/i18n.mineca5.js?url';
-import asset_indexb5e1_js_7 from '../assets/wp-content/plugins/contact-form-7/includes/swv/js/indexb5e1.js?url';
-import asset_indexb5e1_js_8 from '../assets/wp-content/plugins/contact-form-7/includes/js/indexb5e1.js?url';
-import asset_webpack_runtime_minfe70_js_9 from '../assets/wp-content/plugins/elementor/assets/js/webpack.runtime.minfe70.js?url';
-import asset_frontend_modules_minfe70_js_10 from '../assets/wp-content/plugins/elementor/assets/js/frontend-modules.minfe70.js?url';
-import asset_core_minb37e_js_11 from '../assets/wp-includes/js/jquery/ui/core.minb37e.js?url';
-import asset_frontend_minfe70_js_12 from '../assets/wp-content/plugins/elementor/assets/js/frontend.minfe70.js?url';
-import asset_hello_frontend9bf7_js_13 from '../assets/wp-content/themes/hello-elementor/assets/js/hello-frontend9bf7.js?url';
-import asset_wp_polyfill_min2c7c_js_14 from '../assets/wp-includes/js/dist/vendor/wp-polyfill.min2c7c.js?url';
-import asset_indexb5e1_js_15 from '../assets/wp-content/plugins/contact-form-7/modules/recaptcha/indexb5e1.js?url';
-import asset_general_min31dc_js_16 from '../assets/wp-content/plugins/essential-addons-for-elementor-lite/assets/front-end/js/view/general.min31dc.js?url';
-import asset_hoverIntent_min3e5a_js_17 from '../assets/wp-includes/js/hoverIntent.min3e5a.js?url';
-import asset_maxmegamenuef85_js_18 from '../assets/wp-content/plugins/megamenu/js/maxmegamenuef85.js?url';
-import asset_float42a0_js_19 from '../assets/wp-content/plugins/gtranslate/js/float42a0.js?url';
-import asset_wp_emoji_release_min_js_20 from '../assets/wp-includes/js/wp-emoji-release.min.js?url';
-import asset_wp_emoji_loader_min_js_21 from '../assets/wp-includes/js/wp-emoji-loader.min.js?url';
 
 export default function Welcome() {
-    useEffect(() => {
-        window.dispatchEvent(new Event('resize'));
-        setTimeout(() => {
-            window.dispatchEvent(new Event('resize'));
-        }, 500);
-    }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    return (
-        <Layout>
-            <div dangerouslySetInnerHTML={{ __html: `
-			<div data-elementor-type="wp-page" data-elementor-id="416" class="elementor elementor-416">
-				<a class="elementor-element elementor-element-6e668bc e-flex e-con-boxed magic-button-disabled-no e-con e-parent" data-id="6e668bc" data-element_type="container" data-e-type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}" href="/homepage">
-					<div class="e-con-inner">
-				<div class="elementor-element elementor-element-a428b1c animated-slow elementor-widget elementor-widget-image" data-id="a428b1c" data-element_type="widget" data-e-type="widget" data-settings="{&quot;_animation&quot;:&quot;none&quot;}" data-widget_type="image.default">
-															<img fetchpriority="high" decoding="async" width="800" height="229" src="${asset_Bulk_Flex_Brand_white_1024x293_png_1}" class="attachment-large size-large wp-image-1321" alt="" srcset="${asset_Bulk_Flex_Brand_white_1024x293_png_1} 1024w, ${asset_Bulk_Flex_Brand_white_300x86_png_2} 300w, ${asset_Bulk_Flex_Brand_white_768x220_png_3} 768w, ${asset_Bulk_Flex_Brand_white_768x220_png_3} 1536w, ${asset_Bulk_Flex_Brand_white_768x220_png_3} 1654w" sizes="(max-width: 800px) 100vw, 800px">															</div>
-				<div class="elementor-element elementor-element-d5d825d elementor-widget elementor-widget-html" data-id="d5d825d" data-element_type="widget" data-e-type="widget" data-widget_type="html.default">
-					<script>
-setTimeout(function(){
-   window.location.href = "../homepage/index.html";
-}, 3000);
-</script>				</div>
-				<div class="elementor-element elementor-element-c267445 animated-slow elementor-invisible elementor-widget elementor-widget-image" data-id="c267445" data-element_type="widget" data-e-type="widget" data-settings="{&quot;_animation&quot;:&quot;bounceInUp&quot;}" data-widget_type="image.default">
-															<img decoding="async" width="800" height="114" src="${asset_simplified_text_1024x146_png_4}" class="attachment-large size-large wp-image-1156" alt="" srcset="${asset_simplified_text_1024x146_png_4} 1024w, ${asset_simplified_text_1024x146_png_4} 300w, ${asset_simplified_text_1024x146_png_4} 768w, ${asset_simplified_text_1024x146_png_4} 1186w" sizes="(max-width: 800px) 100vw, 800px">															</div>
-					</div>
-				</a>
-				</div>
-		<script type="speculationrules">
-{"prefetch":[{"source":"document","where":{"and":[{"href_matches":"/*"},{"not":{"href_matches":["/wp-*.php","/wp-admin/*","/wp-content/uploads/*","/wp-content/*","/wp-content/plugins/*","/wp-content/themes/hello-elementor/*","/*\\?(.+)"]}},{"not":{"selector_matches":"a[rel~=\"nofollow\"]"}},{"not":{"selector_matches":".no-prefetch, .no-prefetch a"}}]},"eagerness":"conservative"}]}
-</script>
-<div class="gtranslate_wrapper" id="gt-wrapper-71535639"></div>			<script>
-				const lazyloadRunObserver = () => {
-					const lazyloadBackgrounds = document.querySelectorAll( \`.e-con.e-parent:not(.e-lazyloaded)\` );
-					const lazyloadBackgroundObserver = new IntersectionObserver( ( entries ) => {
-						entries.forEach( ( entry ) => {
-							if ( entry.isIntersecting ) {
-								let lazyloadBackground = entry.target;
-								if( lazyloadBackground ) {
-									lazyloadBackground.classList.add( 'e-lazyloaded' );
-								}
-								lazyloadBackgroundObserver.unobserve( entry.target );
-							}
-						});
-					}, { rootMargin: '200px 0px 200px 0px' } );
-					lazyloadBackgrounds.forEach( ( lazyloadBackground ) => {
-						lazyloadBackgroundObserver.observe( lazyloadBackground );
-					} );
-				};
-				const events = [
-					'DOMContentLoaded',
-					'elementor/lazyload/observe',
-				];
-				events.forEach( ( event ) => {
-					document.addEventListener( event, lazyloadRunObserver );
-				} );
-			</script>
-			<script id="wp-hooks-js" src="${asset_hooks_min394d_js_5}"></script>
-<script id="wp-i18n-js" src="${asset_i18n_mineca5_js_6}"></script>
-<script id="wp-i18n-js-after">
-wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
-//# sourceURL=wp-i18n-js-after
-</script>
-<script id="swv-js" src="${asset_indexb5e1_js_7}"></script>
-<script id="contact-form-7-js-before">
-var wpcf7 = {
-    "api": {
-        "root": "https:\/\/bulkflex.com\/wp-json\/",
-        "namespace": "contact-form-7\/v1"
-    }
-};
-//# sourceURL=contact-form-7-js-before
-</script>
-<script id="contact-form-7-js" src="${asset_indexb5e1_js_8}"></script>
-<script id="elementor-webpack-runtime-js" src="${asset_webpack_runtime_minfe70_js_9}"></script>
-<script id="elementor-frontend-modules-js" src="${asset_frontend_modules_minfe70_js_10}"></script>
-<script id="jquery-ui-core-js" src="${asset_core_minb37e_js_11}"></script>
-<script id="elementor-frontend-js-extra">
-var EAELImageMaskingConfig = {"svg_dir_url":"/wp-content/plugins/essential-addons-for-elementor-lite/assets/front-end/img/image-masking/svg-shapes/"};
-//# sourceURL=elementor-frontend-js-extra
-</script>
-<script id="elementor-frontend-js-before">
-var elementorFrontendConfig = {"environmentMode":{"edit":false,"wpPreview":false,"isScriptDebug":false},"i18n":{"shareOnFacebook":"Share on Facebook","shareOnTwitter":"Share on Twitter","pinIt":"Pin it","download":"Download","downloadImage":"Download image","fullscreen":"Fullscreen","zoom":"Zoom","share":"Share","playVideo":"Play Video","previous":"Previous","next":"Next","close":"Close","a11yCarouselPrevSlideMessage":"Previous slide","a11yCarouselNextSlideMessage":"Next slide","a11yCarouselFirstSlideMessage":"This is the first slide","a11yCarouselLastSlideMessage":"This is the last slide","a11yCarouselPaginationBulletMessage":"Go to slide"},"is_rtl":false,"breakpoints":{"xs":0,"sm":480,"md":768,"lg":1025,"xl":1440,"xxl":1600},"responsive":{"breakpoints":{"mobile":{"label":"Mobile Portrait","value":767,"default_value":767,"direction":"max","is_enabled":true},"mobile_extra":{"label":"Mobile Landscape","value":880,"default_value":880,"direction":"max","is_enabled":false},"tablet":{"label":"Tablet Portrait","value":1024,"default_value":1024,"direction":"max","is_enabled":true},"tablet_extra":{"label":"Tablet Landscape","value":1200,"default_value":1200,"direction":"max","is_enabled":false},"laptop":{"label":"Laptop","value":1366,"default_value":1366,"direction":"max","is_enabled":false},"widescreen":{"label":"Widescreen","value":2400,"default_value":2400,"direction":"min","is_enabled":false}},"hasCustomBreakpoints":false},"version":"3.35.8","is_static":false,"experimentalFeatures":{"e_font_icon_svg":true,"additional_custom_breakpoints":true,"container":true,"e_optimized_markup":true,"hello-theme-header-footer":true,"e_pro_free_trial_popup":true,"nested-elements":true,"home_screen":true,"global_classes_should_enforce_capabilities":true,"e_variables":true,"cloud-library":true,"e_opt_in_v4_page":true,"e_components":true,"e_interactions":true,"e_editor_one":true,"import-export-customization":true},"urls":{"assets":"https:\/\/bulkflex.com\/wp-content\/plugins\/elementor\/assets\/","ajaxurl":"https:\/\/bulkflex.com\/wp-admin\/admin-ajax.php","uploadUrl":"https:\/\/bulkflex.com\/wp-content\/uploads"},"nonces":{"floatingButtonsClickTracking":"8676306772"},"swiperClass":"swiper","settings":{"page":[],"editorPreferences":[]},"kit":{"active_breakpoints":["viewport_mobile","viewport_tablet"],"global_image_lightbox":"yes","lightbox_enable_counter":"yes","lightbox_enable_fullscreen":"yes","lightbox_enable_zoom":"yes","lightbox_enable_share":"yes","lightbox_title_src":"title","lightbox_description_src":"description","hello_header_logo_type":"title","hello_header_menu_layout":"horizontal","hello_footer_logo_type":"logo"},"post":{"id":416,"title":"Welcome%20-%20Bulk%20Flex%20Private%20Limited","excerpt":"","featuredImage":false}};
-//# sourceURL=elementor-frontend-js-before
-</script>
-<script id="elementor-frontend-js" src="${asset_frontend_minfe70_js_12}"></script>
-<script id="hello-theme-frontend-js" src="${asset_hello_frontend9bf7_js_13}"></script>
-<script id="google-recaptcha-js" src="../../www.google.com/recaptcha/api66f1.html?render=6Le0t6QsAAAAAOAVo6Idz_pctlDsTTpRUNC13AQp&amp;ver=3.0"></script>
-<script id="wp-polyfill-js" src="${asset_wp_polyfill_min2c7c_js_14}"></script>
-<script id="wpcf7-recaptcha-js-before">
-var wpcf7_recaptcha = {
-    "sitekey": "6Le0t6QsAAAAAOAVo6Idz_pctlDsTTpRUNC13AQp",
-    "actions": {
-        "homepage": "homepage",
-        "contactform": "contactform"
-    }
-};
-//# sourceURL=wpcf7-recaptcha-js-before
-</script>
-<script id="wpcf7-recaptcha-js" src="${asset_indexb5e1_js_15}"></script>
-<script id="eael-general-js-extra">
-var localize = {"ajaxurl":"/wp-admin/admin-ajax.php","nonce":"59c2234202","i18n":{"added":"Added ","compare":"Compare","loading":"Loading..."},"eael_translate_text":{"required_text":"is a required field","invalid_text":"Invalid","billing_text":"Billing","shipping_text":"Shipping","fg_mfp_counter_text":"of"},"page_permalink":"/welcome/","cart_redirectition":"","cart_page_url":"","el_breakpoints":{"mobile":{"label":"Mobile Portrait","value":767,"default_value":767,"direction":"max","is_enabled":true},"mobile_extra":{"label":"Mobile Landscape","value":880,"default_value":880,"direction":"max","is_enabled":false},"tablet":{"label":"Tablet Portrait","value":1024,"default_value":1024,"direction":"max","is_enabled":true},"tablet_extra":{"label":"Tablet Landscape","value":1200,"default_value":1200,"direction":"max","is_enabled":false},"laptop":{"label":"Laptop","value":1366,"default_value":1366,"direction":"max","is_enabled":false},"widescreen":{"label":"Widescreen","value":2400,"default_value":2400,"direction":"min","is_enabled":false}}};
-//# sourceURL=eael-general-js-extra
-</script>
-<script id="eael-general-js" src="${asset_general_min31dc_js_16}"></script>
-<script id="hoverIntent-js" src="${asset_hoverIntent_min3e5a_js_17}"></script>
-<script id="megamenu-js" src="${asset_maxmegamenuef85_js_18}"></script>
-<script id="gt_widget_script_71535639-js-before">
-window.gtranslateSettings = /* document.write */ window.gtranslateSettings || {};window.gtranslateSettings['71535639'] = {"default_language":"en","languages":["ar","zh-CN","en","fr","de","it","pt","ru","es"],"url_structure":"none","flag_style":"3d","wrapper_selector":"#gt-wrapper-71535639","alt_flags":[],"float_switcher_open_direction":"top","switcher_horizontal_position":"right","switcher_vertical_position":"top","custom_css":".gt_float_switcher .gt-selected .gt-current-lang {\r\n    padding: 2px 9px;\r\n    color: #333;\r\n    font-weight: 600;\r\n    font-size: 14px;\r\n}\r\n\r\n.gt_float_switcher .gt_options a {\r\n    display: block;\r\n    text-decoration: none;\r\n    padding: 6px 15px;\r\n    color: #444;\r\n    transition: color .4s linear;\r\n    font-size: 14px;\r\n}\r\n\r\n.gt_float_switcher {\r\n       border-radius: 11px !important;\r\n}\r\n\r\n.gt_float_switcher img {\r\n    width: 28px !important;\r\n    margin: 3px 5px 3px 0 !important;\r\n}\r\n\r\n.gt_float_switcher {\r\n\tfont-family: Arial;\r\n\tfont-size: 20px;\r\n\tborder-radius: 2px;\r\n\tcolor: #555;\r\n\tdisplay: inline-block;\r\n\tline-height: 20px;\r\n\tbox-shadow: rgba(0,0,0,0.15) 0 5px 15px;\r\n\tbackground: #fff;\r\n\toverflow: hidden;\r\n\ttransition: all .5s cubic-bezier(0.4, 0, 1, 1);\r\n}","flags_location":"\/wp-content\/plugins\/gtranslate\/flags\/"};
-//# sourceURL=gt_widget_script_71535639-js-before
-</script><script src="${asset_float42a0_js_19}" data-no-optimize="1" data-no-minify="1" data-gt-orig-url="/welcome/" data-gt-orig-domain="bulkflex.com" data-gt-widget-id="71535639" defer=""></script><script id="wp-emoji-settings" type="application/json">
-{"baseUrl":"https://s.w.org/images/core/emoji/17.0.2/72x72/","ext":".png","svgUrl":"https://s.w.org/images/core/emoji/17.0.2/svg/","svgExt":".svg","source":{"concatemoji":"${asset_wp_emoji_release_min_js_20}"}}
-</script>
-<script type="module">
-/*! This file is auto-generated */
-const a=JSON.parse(document.getElementById("wp-emoji-settings").textContent),o=(window._wpemojiSettings=a,"wpEmojiSettingsSupports"),s=["flag","emoji"];function i(e){try{var t={supportTests:e,timestamp:(new Date).valueOf()};sessionStorage.setItem(o,JSON.stringify(t))}catch(e){}}function c(e,t,n){e.clearRect(0,0,e.canvas.width,e.canvas.height),e.fillText(t,0,0);t=new Uint32Array(e.getImageData(0,0,e.canvas.width,e.canvas.height).data);e.clearRect(0,0,e.canvas.width,e.canvas.height),e.fillText(n,0,0);const a=new Uint32Array(e.getImageData(0,0,e.canvas.width,e.canvas.height).data);return t.every((e,t)=>e===a[t])}function p(e,t){e.clearRect(0,0,e.canvas.width,e.canvas.height),e.fillText(t,0,0);var n=e.getImageData(16,16,1,1);for(let e=0;e<n.data.length;e++)if(0!==n.data[e])return!1;return!0}function u(e,t,n,a){switch(t){case"flag":return n(e,"\ud83c\udff3\ufe0f\u200d\u26a7\ufe0f","\ud83c\udff3\ufe0f\u200b\u26a7\ufe0f")?!1:!n(e,"\ud83c\udde8\ud83c\uddf6","\ud83c\udde8\u200b\ud83c\uddf6")&&!n(e,"\ud83c\udff4\udb40\udc67\udb40\udc62\udb40\udc65\udb40\udc6e\udb40\udc67\udb40\udc7f","\ud83c\udff4\u200b\udb40\udc67\u200b\udb40\udc62\u200b\udb40\udc65\u200b\udb40\udc6e\u200b\udb40\udc67\u200b\udb40\udc7f");case"emoji":return!a(e,"\ud83e\u1fac8")}return!1}function f(e,t,n,a){let r;const o=(r="undefined"!=typeof WorkerGlobalScope&&self instanceof WorkerGlobalScope?new OffscreenCanvas(300,150):document.createElement("canvas")).getContext("2d",{willReadFrequently:!0}),s=(o.textBaseline="top",o.font="600 32px Arial",{});return e.forEach(e=>{s[e]=t(o,e,n,a)}),s}function r(e){var t=document.createElement("script");t.src=e,t.defer=!0,document.head.appendChild(t)}a.supports={everything:!0,everythingExceptFlag:!0},new Promise(t=>{let n=function(){try{var e=JSON.parse(sessionStorage.getItem(o));if("object"==typeof e&&"number"==typeof e.timestamp&&(new Date).valueOf()<e.timestamp+604800&&"object"==typeof e.supportTests)return e.supportTests}catch(e){}return null}();if(!n){if("undefined"!=typeof Worker&&"undefined"!=typeof OffscreenCanvas&&"undefined"!=typeof URL&&URL.createObjectURL&&"undefined"!=typeof Blob)try{var e="postMessage("+f.toString()+"("+[JSON.stringify(s),u.toString(),c.toString(),p.toString()].join(",")+"));",a=new Blob([e],{type:"text/javascript"});const r=new Worker(URL.createObjectURL(a),{name:"wpTestEmojiSupports"});return void(r.onmessage=e=>{i(n=e.data),r.terminate(),t(n)})}catch(e){}i(n=f(s,u,c,p))}t(n)}).then(e=>{for(const n in e)a.supports[n]=e[n],a.supports.everything=a.supports.everything&&a.supports[n],"flag"!==n&&(a.supports.everythingExceptFlag=a.supports.everythingExceptFlag&&a.supports[n]);var t;a.supports.everythingExceptFlag=a.supports.everythingExceptFlag&&!a.supports.flag,a.supports.everything||((t=a.source||{}).concatemoji?r(t.concatemoji):t.wpemoji&&t.twemoji&&(r(t.twemoji),r(t.wpemoji)))});
-//# sourceURL=${asset_wp_emoji_loader_min_js_21}
-</script>
-	
-
-
-
-` }} />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div style={{
+        background: '#072834',
+        minHeight: '70vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 'clamp(32px, 6vw, 64px)',
+        textAlign: 'center'
+      }}>
+        <img
+          src={asset_Bulk_Flex_Brand_white_1024x293_png_1}
+          alt="BulkFlex Industrial Packaging"
+          style={{ maxWidth: '480px', width: '100%', height: 'auto', marginBottom: '24px' }}
+        />
+        <img
+          src={asset_simplified_text_1024x146_png_4}
+          alt="Bulk Sourcing Simplified"
+          style={{ maxWidth: '360px', width: '100%', height: 'auto', marginBottom: '32px' }}
+        />
+        <Link to="/" className="btn-dark" style={{ background: '#00C2A8', color: '#072834', padding: '14px 32px', fontWeight: '800' }}>
+          Explore Homepage →
+        </Link>
+      </div>
+    </Layout>
+  );
 }
